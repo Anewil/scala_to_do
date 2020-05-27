d29a26e75f37af6bae935c9ba24ec38a641c8989
Anewil <bartekwwb@gmail.com>
2020-05-27 19:28:37 +0200

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.i18n.I18nSupport
+import repositories.TaskRepository
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(taskService: TaskRepository, val cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+
+
+  def index = Action { implicit request: Request[AnyContent] =>
+    Redirect(routes.TaskController.index)
+  }
+
+}
---
+++ app/repositories/TaskRepository.scala
@@ -0,0 +1,63 @@
+package repositories
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import models.Task
+
+
+@javax.inject.Singleton
+class TaskRepository @Inject()(database: Database)(implicit ec: ExecutionContext) {
+
+  private val task = {
+    get[Option[Long]]("id") ~
+      get[String]("name") ~
+      get[String]("comments") ~
+      get[Int]("completed") map {
+      case id ~ name ~ comments ~ completed => Task(id, name, comments, completed == 1)
+    }
+  }
+
+  private val DB = database
+
+  def getById(id: Long): Option[Task] = {
+    DB.withConnection { implicit c =>
+      SQL("select * from task where id = {id}").on('id -> id).as(task.singleOpt)
+    }
+  }
+
+
+  def all(): List[Task] = DB.withConnection { implicit c =>
+    SQL("select * from task").as(task *)
+  }
+
+  def create(task: Task) {
+    DB.withConnection { implicit c =>
+      SQL("""
+        insert into task (name, comments, completed) values (
+          {name}, {comments}, {completed}
+        )
+        """).bind(task).executeInsert()
+    }
+  }
+
+  def update(id: Long, task: Task) {
+    DB.withConnection { implicit c =>
+      SQL("""
+        UPDATE task SET name={name}, comments={comments}, completed={completed}
+        WHERE id = {id}
+        """).bind(task.copy(id = Some(id))).executeUpdate()
+    }
+  }
+
+  def delete(id: Long) {
+    DB.withConnection { implicit c =>
+      SQL("""
+        delete from task where id = {id}
+        """).on('id -> id).executeUpdate()
+    }
+  }
+
+}
